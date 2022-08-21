5a712709303f3898d90e6d693f0deb501fb0b4ef
AndreyVolkov <volk_bur-gora@mail.ru>
2022-08-21 17:28:23 +0300

    Задача 1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Есть монотонная последовательность, каждое число встречается ровно то количество раз, какое значение этого числа: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,... 
+//Выведите ряд этой последовательности до N-го значения, желательно использовать рекурсию:
+
+void PrintSequence(int N, int counter = 0, int start = 0)
+{
+    for (int i = 0; i < start; i++)
+    {
+        counter++;
+        Console.Write($"{start}, ");
+        if (counter == N) return;
+    }
+    PrintSequence(N, counter, start + 1);
+
+}
+
+PrintSequence(12);
+
